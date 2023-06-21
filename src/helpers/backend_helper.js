d4f6a5dd05d5cba45e454aa30896b5b00f54162a
heli27p <121236041+heli27p@users.noreply.github.com>
2023-06-21 21:23:22 +0530

    newdashboard

--- src/helpers/backend_helper.js
+++ src/helpers/backend_helper.js
@@ -5,8 +5,23 @@ import * as url from "./url_helper";
 // Register Method
 const postRegister = data => post(url.POST_REGISTER, data);
 const postLogin = data => post(url.POST_LOGIN, data);
+// DASHBOARD ITEMS COUNT
+const getSubcriptions = () => get(url.GET_SUBSCRIPTIONS);
+const getStudents = () => get(url.GET_STUDENTS);
+const getVideos = () => get(url.GET_VIDEOS);
+const getRevenue = () => get(url.GET_REVENUE);
+const getRatings = () => get(url.GET_RATINGS);
+const getEarnings = () => get(url.GET_EARNINGS);
 
 export {
   postRegister,
+  getSubcriptions,
+  getStudents,
+  getVideos,
+  getRevenue,
+  getRatings,
+  getEarnings,
+  postRegister,
   postLogin
 };
+
--- src/helpers/url_helper.js
+++ src/helpers/url_helper.js
@@ -1,4 +1,15 @@
 //REGISTER
 export const POST_REGISTER = "/v1/signup/basic";
-
 export const POST_LOGIN = "/v1/login/basic";
+//DASHBOARD
+export const GET_SUBSCRIPTIONS = "/v1/api/dashboard/events/subscriptions/count";
+
+export const GET_STUDENTS = "/v1/api/dashboard/students/count";
+
+export const GET_VIDEOS = "/v1/api/dashboard/video/count";
+
+export const GET_REVENUE = "/v1/api/dashboard/revenue/count";
+
+export const GET_RATINGS = "/v1/api/dashboard/overallratings";
+
+export const GET_EARNINGS = "/v1/api/dashboard/totalEarnings";
