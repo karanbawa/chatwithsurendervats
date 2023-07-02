6358266aa7dd80ca74cf4ead30b2e318c4671d20
karanbawa <karan.bawa2@gmail.com>
2023-07-03 00:51:35 +0530

    fix: status code

--- src/pages/Books/index.js
+++ src/pages/Books/index.js
@@ -2,10 +2,7 @@ import React, { useEffect, useMemo, useState } from "react"
 import PropTypes from "prop-types"
 import { Link, useHistory, useNavigate } from "react-router-dom"
 import { isEmpty } from "lodash"
-// import "../../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import TableContainer from "../../components/Common/TableContainer" 
-import * as Yup from "yup"
-import { useFormik } from "formik"
 
 //import components
 import Breadcrumbs from "../../components/Common/Breadcrumb"
